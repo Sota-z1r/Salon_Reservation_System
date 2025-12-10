d04b08679ac8435cdc0e763723653d930cbd9a5b
皆神美由紀 <minakamimiyuki@minakamimiyukinoMacBook-Pro.local>
2025-12-10 19:53:37 +0900

    json読み込み

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from app import create_app
+import os
+import json
+
+# credentials.json を書き出す
+cred_json = os.environ.get("GOOGLE_CREDENTIALS_JSON")
+if cred_json:
+    with open("credentials.json", "w") as f:
+        f.write(cred_json)
+
+# token.json を書き出す
+token_json = os.environ.get("GOOGLE_TOKEN_JSON")
+if token_json:
+    with open("token.json", "w") as f:
+        f.write(token_json)
+
 
 app = create_app()
 
